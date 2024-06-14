a71f1848968176bb7cf3a3951774eb0231de6296
Mac <mac@Macs-MacBook-Pro.local>
2024-06-14 11:27:57 +0700

    there commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/ssconfig")();
+require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
